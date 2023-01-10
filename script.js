abfc0addbf97ca36dc4b53bbbfea2d4ae27ca016
Pedro Ramos <opedroramosbr@gmail.com>
2023-01-10 10:46:09 -0300

    000

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 setTimeout(
     function tempo() {
         var elemento = document.querySelector('#botaopai')
-        //var elemento11 = document.querySelector('#pagamentos')
-        var elemento13 = document.querySelector('#vagas')
+        var elemento11 = document.querySelector('#pagamentos')
+        //var elemento13 = document.querySelector('#vagas')
         //var elemento20 = document.querySelector('#objeções')
         var elemento21 = document.querySelector('#pitchhh')
         //var elemento22 = document.querySelector('#addcarrinho')
@@ -10,12 +10,12 @@ setTimeout(
 
 
         elemento.style.display = "flex";
-        //elemento11.style.display = "flex";
-        elemento13.style.display = "flex";
+        elemento11.style.display = "flex";
+        //elemento13.style.display = "flex";
         //elemento20.style.display = "flex"
         elemento21.style.display = "flex"
         //elemento22.style.display = "block"
         //elemento18.style.display = "flex";
 
 
-    }, /*tempo em milisegundos*/ 1440000)
+    }, /*tempo em milisegundos*/ 500)
